eccba498cb97416fccba39d688ad022f6a5db1bf
Andrew Thappa <adthappa@gmail.com>
2015-01-23 12:12:40 -0500

    add beginning of the insert and remove logic

---
+++ human_v_ai.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+'''
+Game of Sticks:
+Human v. Human Sticks
+Hat Tip: http://nifty.stanford.edu/2014/laaksonen-vihavainen-game-of-sticks/handout.html
+'''
+
+import random
+
+"Global Variables"
+prompt = '>>> ' 
+btw_1_3 = 'Please pick a number beteween 1 and 3.'
+#player_1 = 0
+#player_2 = 0
+
+raw_input("Welcome to the game of sticks! Press <Enter> to continue...")
+
+
+
+
+print("How many sticks are there on the table initially (10-100)?")
+num_sticks = int(raw_input(prompt))
+print("Ok, there are %s sticks on the board.") % (num_sticks)
+print("|")*num_sticks
+
+
+stick_dict={}
+for number in range(1,num_sticks+1):
+	stick_dict["hat{0}".format(number)]=[[1,2,3]]
+
+print d
+
+
+
+'''
+while num_sticks!=0:
+	#player 1
+	while True:
+		print "Player 1: How many sticks do you take (1-3)?"
+		player_1 =  int(raw_input(prompt))
+		
+		if 1 <= player_1 <= 3:
+			break
+		else:
+			print (btw_1_3)
+
+	num_sticks = num_sticks - player_1
+	if num_sticks == 1:
+		print("Ok, there is %s stick on the board.") % (num_sticks)
+		print("|")*num_sticks
+
+	elif num_sticks == 0:
+		print("Player 1, you lose.")
+		break
+	else:
+		print("Ok, there are %s sticks on the board.") % (num_sticks)
+		print("|")*num_sticks
+
+'''
